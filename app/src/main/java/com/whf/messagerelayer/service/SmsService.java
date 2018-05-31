3b4badd3704ff1f777930df2c1785ea13a6934c8
Fish <1158410592@qq.com>
2018-05-31 09:59:14 +0800

    Update SmsService.java
    转发消息时带上原始短信的发送方号码，方便识别

--- app/src/main/java/com/whf/messagerelayer/service/SmsService.java
+++ app/src/main/java/com/whf/messagerelayer/service/SmsService.java
@@ -45,14 +45,14 @@ public class SmsService extends IntentService {
         } else if (keySet.size() != 0 && contactList.size() == 0) {//仅有关键字规则
             for (String key : keySet) {
                 if (content.contains(key)) {
-                    relayMessage(content);
+                    relayMessage(content, mobile);
                     break;
                 }
             }
         } else if (keySet.size() == 0 && contactList.size() != 0) {//仅有手机号规则
             for (Contact contact : contactList) {
                 if (contact.getContactNum().equals(mobile)) {
-                    relayMessage(content);
+                    relayMessage(content, mobile);
                     break;
                 }
             }
@@ -62,7 +62,7 @@ public class SmsService extends IntentService {
                 if (contact.getContactNum().equals(mobile)) {
                     for (String key : keySet) {
                         if (content.contains(key)) {
-                            relayMessage(content);
+                            relayMessage(content, mobile);
                             break out;
                         }
                     }
@@ -71,9 +71,12 @@ public class SmsService extends IntentService {
         }
     }
 
-    private void relayMessage(String content) {
+    private void relayMessage(String content, String mobile) {
         String suffix = mNativeDataManager.getContentSuffix();
         String prefix = mNativeDataManager.getContentPrefix();
+        if(mobile!=null){
+            content = content + "\nFrom: " + mobile;
+        }
         if(suffix!=null){
             content = content+suffix;
         }
